957f6fb9ef105f5bafb8a6555fe104a8ca605725
Ivan Krivonos <ivan.krivonos@bitfury.com>
2021-11-26 02:16:47 +0300

    text animation

---
+++ source/js/modules/common/text-animation.js
@@ -0,0 +1,38 @@
+export default class TextAnimation {
+  constructor(delay, duration) {
+    this.delay = delay;
+    this.duration = duration || this.delay * 3;
+  }
+  init(node) {
+    node.classList.add(`animated-text`);
+  }
+
+  splitByChar(node, globalDelay) {
+    if (node.classList.contains(`animated-text_init`)) {
+      return;
+    }
+    node.innerHTML = Array.from(node.textContent).map((char) => `<span>${char}</span>`).join(``);
+    node.classList.add(`animated-text_init`);
+    const wrappedNode = document.createElement(`div`);
+    let lineCount = 0;
+    node.querySelectorAll(`span`).forEach((span) => {
+      let line = wrappedNode.querySelector(`span[data-offset="${span.offsetTop}"]`);
+      if (!line) {
+        line = document.createElement(`span`);
+        line.dataset.offset = span.offsetTop;
+        line.classList.add(`animated-text__line`);
+        wrappedNode.appendChild(line);
+        lineCount++;
+      }
+      span.classList.add(`animated-text__char`);
+      span.style.animationDelay = `${Math.random() * this.delay + this.duration * (lineCount - 1) + globalDelay}ms`;
+      span.style.animationDuration = `${this.duration}ms`;
+      line.appendChild(span.cloneNode(true));
+    });
+    wrappedNode.querySelectorAll(`span`).forEach((span) => {
+      delete span.dataset.offset;
+    });
+    node.innerHTML = wrappedNode.innerHTML;
+  }
+
+}
--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash/throttle';
+import TextAnimation from "./common/text-animation";
 
 export default class FullPageScroll {
   constructor() {
@@ -8,16 +9,27 @@ export default class FullPageScroll {
 
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = Array.from(document.querySelectorAll(`.page-header__menu .js-menu-link`));
-
+    this.textAnimation = new TextAnimation(150);
     this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
+    this.animationNodes = [
+      `.intro__title, .intro__label, .intro__date`,
+      `.slider__item-title`,
+      `.prizes__title,  .prizes__desc b`,
+      `.rules__title`,
+      `.game__title`
+    ];
   }
 
   init() {
     document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, {trailing: true}));
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
-
+    this.screenElements.forEach((screen, idx) => {
+      screen.querySelectorAll(this.animationNodes[idx]).forEach((node) => {
+        this.textAnimation.init(node);
+      });
+    });
     this.onUrlHashChanged();
   }
 
@@ -53,14 +65,19 @@ export default class FullPageScroll {
 
   changeVisibilityDisplay() {
     const currentActiveScreen = Array.from(this.screenElements).find((screen) => screen.classList.contains(`active`));
+
     const toggleScreen = () => {
       this.screenElements.forEach((screen) => {
         screen.classList.add(`screen--hidden`);
         screen.classList.remove(`active`);
       });
-      this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
+      const activeScreenNode = this.screenElements[this.activeScreen];
+      activeScreenNode.classList.remove(`screen--hidden`);
       setTimeout(() => {
-        this.screenElements[this.activeScreen].classList.add(`active`);
+        activeScreenNode.classList.add(`active`);
+        activeScreenNode.querySelectorAll(this.animationNodes[this.activeScreen]).forEach((node, idx) => {
+          this.textAnimation.splitByChar(node, 300 * idx);
+        });
       }, 100);
     };
     if (Array.from(this.screenElements).indexOf(currentActiveScreen) === 1) {
