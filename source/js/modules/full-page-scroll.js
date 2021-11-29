55345b0b97630609a17e9b1c6097bbf86b871209
Ivan Krivonos <ivan.krivonos@bitfury.com>
2021-11-29 15:05:25 +0300

    add result animation

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -7,7 +7,7 @@ export default class FullPageScroll {
     this.scrollFlag = true;
     this.timeout = null;
 
-    this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
+    this.screenElements = document.querySelectorAll(`.screen`);
     this.menuElements = Array.from(document.querySelectorAll(`.page-header__menu .js-menu-link`));
     this.textAnimation = new TextAnimation(150);
     this.activeScreen = 0;
